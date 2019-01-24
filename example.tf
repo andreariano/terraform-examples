245492e35d229b16ef82b64c13669c135134291a
Andre Ariano <andreariano@yahoo.com.br>
2019-01-24 11:53:28 -0200

    Modules example using consul

--- example.tf
+++ example.tf
@@ -4,15 +4,8 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-b374d5a5"
-  instance_type = "t2.micro"
-}
+module "consul" {
+  source = "hashicorp/consul/aws"
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
-
-output "ip" {
-  value = "${aws_eip.ip.public_ip}"
+  num_servers = "3"
 }
